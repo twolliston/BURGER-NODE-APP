0189ebff6ff365f3de007394b81a2fce600b14b5
Timothy Wolliston <Twolliston@Admins-MacBook-Pro.local>
2018-08-25 22:57:42 -0400

    1.) Added Boiler Plate template which includes the follwoing file connection.js, orm.js, burgers_controller.js, schema.sql, seeds.sql, burger.js, main.handlebars, index.handlebars and server.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Happy Meal', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon-egg & cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
